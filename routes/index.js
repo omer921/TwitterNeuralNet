7fc5852063f1065e12c85e431d3926d2b67381c0
Omer Winrauke <omer921@gmail.com>
2017-04-23 16:18:03 -0400

    Added a post endpoint that takes a twitter handle and runs the model to determine if the user is a bot

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Twitter Bot Detector' });
+  console.log("im here")
+  res.render('index', { title: 'Twitter Bot Detector'});
+});
+
+
+router.post('/check', function(req, res, next) {
+  var userName = req.body.handle;
+  res.render('index', { title: 'Twitter Bot Detector'});
 });
 
 module.exports = router;

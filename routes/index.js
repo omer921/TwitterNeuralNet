60511a600a91d7f22788d62d6e9863f61311741e
Omer Winrauke <omer921@gmail.com>
2017-04-23 15:54:56 -0400

    Added button and input field to the landing page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Omer' });
+  res.render('index', { title: 'Twitter Bot Detector' });
 });
 
 module.exports = router;

07834112a3ed30625c6924a16a7dc88d4a3f334a
Josiah MacDonald <josiahhalemacdonald@gmail.com>
2017-04-24 19:02:09 -0400

    errything

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,48 @@
 var express = require('express');
 var router = express.Router();
+var PythonShell = require('python-shell');
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   console.log("im here")
+
   res.render('index', { title: 'Twitter Bot Detector'});
 });
 
 
 router.post('/check', function(req, res, next) {
   var userName = req.body.handle;
-  res.render('index', { title: 'Twitter Bot Detector'});
+
+  var options = {
+    args: [userName]
+  };
+
+  var pyshell = new PythonShell('Classification.py', options);
+
+
+  pyshell.on('message', function (message) {
+  // received a message sent from the Python script (a simple "print" statement)
+    var data = JSON.parse(message);
+    var result = [];
+    for(key in data) {
+      var screen = key;
+      var obj = {
+        'screen': screen
+      }
+      obj['data'] = {};
+      for(k in data[key]) {
+
+        obj['data'][k] = data[key][k];
+        obj['data'][k]['title'] = k
+      }
+      result.push(obj)
+
+    }
+    console.log(JSON.stringify(result));
+    res.render('index', { title: 'Twitter Bot Detector', data: result});
+  });
 });
 
 module.exports = router;

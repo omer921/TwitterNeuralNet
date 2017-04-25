67aeb0c2a8479be0c23402a6762fd8232fd5a98a
Josiah MacDonald <josiahhalemacdonald@gmail.com>
2017-04-24 21:18:04 -0400

    logs reported correctly...?

--- routes/index.js
+++ routes/index.js
@@ -24,24 +24,31 @@ router.post('/check', function(req, res, next) {
 
   pyshell.on('message', function (message) {
   // received a message sent from the Python script (a simple "print" statement)
-    var data = JSON.parse(message);
-    var result = [];
-    for(key in data) {
-      var screen = key;
-      var obj = {
-        'screen': screen
-      }
-      obj['data'] = {};
-      for(k in data[key]) {
+    try {
+      var data = JSON.parse(message);
+      var result = [];
+      for(key in data) {
+        var screen = key;
+        var obj = {
+          'screen': screen
+        }
+        obj['data'] = {};
+        for(k in data[key]) {
 
-        obj['data'][k] = data[key][k];
-        obj['data'][k]['title'] = k
-      }
-      result.push(obj)
+          obj['data'][k] = data[key][k];
+          obj['data'][k]['title'] = k
+        }
+        result.push(obj)
 
+      }
+      console.log(JSON.stringify(result));
+      res.render('index', { title: 'Twitter Bot Detector', data: result});
+    } catch (e) {
+      console.log(message);
+      console.log(e);
+      res.render('index', { title: 'An error was caught and logged'})
     }
-    console.log(JSON.stringify(result));
-    res.render('index', { title: 'Twitter Bot Detector', data: result});
+
   });
 });
 
